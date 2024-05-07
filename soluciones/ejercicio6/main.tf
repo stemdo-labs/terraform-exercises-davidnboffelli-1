181cce942b3647b6fee016733cb65d4275b72544
David Boffelli <dnazareno@stemdo.io>
2024-05-07 11:39:47 +0200

    Subo ejercicio6

---
+++ soluciones/ejercicio6/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.102.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "ejerciciosanteriores" {
+  source = "github.com/stemdo-labs/terraform-exercises-davidnboffelli-1/soluciones/ejercicio5/modules/ejerciciosanteriores"
+  owner_tag = "David"
+  vnet_address_space = var.vnet_address_space
+  environment_tag = "PRO"
+  vnet_name = var.vnet_name
+  existent_resource_group_name = var.existent_resource_group_name
+}
